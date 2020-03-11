60834f518d3fcd7f941ff6afe52d732b1d90532b
GormTheWyrm <gdsoucie@gmail.com>
2020-03-11 11:52:00 -0400

    basic formatting done

---
+++ assetts/script.js
@@ -0,0 +1,26 @@
+console.log("test");
+
+
+
+
+
+// ***************** MAINAPP **************************
+//function to adjust color of active element... 
+    //avtivation or can do thru timeclock directly?
+
+
+
+    // hour, event-name, edit as 3 main compenents within timeblock (all classes)
+    //timeblocks have unique IDs; time of day in letters (seven, eight, one, etc)
+        //events should add span to time block. Hopefully that allows for multiple events to appear on the timeblock
+        //not sure how to edit multiple events...will likely 
+        //...need to find a way to allow users to choose spans...
+        //edit is now a button class
+
+
+//add span upon submit?
+
+
+
+        // MAIN APP************
+        //call function here
